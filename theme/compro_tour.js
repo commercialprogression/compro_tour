2f68640320cc5621076cdcfcd3130f7c43b3c8b1
Chris Keller <chris@commercialprogression.com>
2015-09-04 16:01:19 -0400

    Initial commit

---
+++ theme/compro_tour.js
@@ -0,0 +1,38 @@
+
+(function ($) {
+  Drupal.behaviors.comproTour = {
+    attach: function (context, settings) {
+      var steps = Drupal.settings.compro_tour.steps;
+      var title = Drupal.settings.compro_tour.tour;
+      var link = Drupal.settings.compro_tour.link;
+
+      // Create empty tour.
+      Drupal.settings.tour = {
+        id: title,
+        steps: []
+      };
+
+      // Add steps to tour.
+      for (var i = 0; i < steps.length; i++) {
+        Drupal.settings.tour.steps.push({
+          title: steps[i].title,
+          content: steps[i].content,
+          target: document.querySelector(steps[i].target),
+          placement: steps[i].placement
+        });
+      }
+
+      console.log(link);
+
+      // Rev it up!
+      if (link == false) {
+        hopscotch.startTour(Drupal.settings.tour);
+      }
+    }
+  };
+}(jQuery));
+
+// Function to start tour.
+function startTour() {
+  hopscotch.startTour(Drupal.settings.tour);
+}
